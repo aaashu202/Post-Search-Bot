ad97ff11c2eecfc4e36bc974bb067adf7cc010c7
aaashu202 <167896518+aaashu202@users.noreply.github.com>
2025-05-28 19:44:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,6 +3,6 @@ API_HASH     = "cac582c764755531d7fde92e444a23b7"
 BOT_TOKEN    = ""
 SESSION      = "BQG9a8UAvpAuAqnt-pDQrcPUSxKLv7W5ejwdn0cRlDrJXGLSe3l8QIPEZ3UegMjlQjf1W4UDb_-8PVtr4PUX-GEyJauAvMf2v0G6-jRHBsgRuDQgos4XY8CdwzErATL0wSaqL_YKJW6O8rZnb0t6VBJBBKbmIO47ZaLPBUeWKTtNtrp45vatgqgbJvKmlps3pBw67DHwIhbcwLg_Z7wkGMIPXUUCz0LlXfQiW8JkmxSc_yQJbjXBE0AFTTzIZVyNOcgUWJTsmC4Xp5NlYMmHgTp4dTIZeKDyyIzISmMUMgNoC7l6i0jUaXA_9mgFRSTZkOL2e_Huvv2vrP8QKkGrjOoYFIHCegAAAAHJBgogAQ"
 DATABASE_URI = "mongodb+srv://brainless:Solocrazy123@cluster0.uymhx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-LOG_CHANNEL  = 
-ADMIN        = 
+LOG_CHANNEL  = -1002063983487
+ADMIN        = 6706180358
 CHANNEL      = "cypherixsocity"
